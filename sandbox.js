8eb358ca3137c46a62f071370f30022892a8496c
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-10-04 20:56:30 +0000

    Update files

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+let playButton = document.createElement('button');
+playButton.setAttribute("class", "btn btn-secondary");
+playButton.innerHTML = "Play Tic Tac Toe";
+document.body.appendChild(playButton);
+playButton.addEventListener('click');
